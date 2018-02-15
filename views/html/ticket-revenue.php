0ce80e0869cf8ebfe00ceea549bf2e1261e9e81b
Corey McKrill <corey.mckrill@gmail.com>
2018-02-15 12:05:40 -0800

    Ticket Revenue: Add public UI and refactor WPCS determination
    Instead of using the currency to determine if a transaction went through
    WPCS, this looks at the payment method meta data associated with the
    transaction. This is probably more accurate than the currency method,
    but it also introduces a new issue because some transactions don't have
    a recorded payment method. It seems to only occur for transactions where
    the total amount due was 0, either because the initial price of the
    ticket was free, or a discount was applied that made the ticket free.
    So, this also adds a new group to the compiled results, for transactions
    without a payment method.

--- views/html/ticket-revenue.php
+++ views/html/ticket-revenue.php
@@ -9,16 +9,18 @@ defined( 'WPINC' ) || die();
 /** @var \DateTime $start_date */
 /** @var \DateTime $end_date */
 /** @var string $wordcamp_name */
-/** @var array $wpcs */
-/** @var array $non_wpcs */
-/** @var array $total */
+/** @var array $data */
 
 $asterisk2 = false;
+$groups    = 0;
 ?>
 
-<?php if ( ! empty( $wpcs['net_revenue_by_currency'] ) ) : ?>
+<?php foreach ( $data as $key => $group ) : ?>
+	<?php if ( empty( $group['gross_revenue_by_currency'] ) ) continue; ?>
+	<?php if ( 'total' === $key && $groups < 2 ) continue; ?>
+
 	<h3>
-		WPCS Ticket Revenue
+		<?php echo esc_html( $group['label'] ); ?>
 		<?php if ( $wordcamp_name ) : ?>
 			for <?php echo esc_html( $wordcamp_name ); ?>
 		<?php endif; ?>
@@ -29,70 +31,13 @@ $asterisk2 = false;
 		<?php endif; ?>
 	</h3>
 
-	<p class="description">These numbers are based on the assumption that every camp using a currency that is supported by PayPal is running its ticket sales through WPCS.</p>
+	<?php if ( $group['description'] ) : ?>
+		<p class="description"><?php echo wp_kses_post( $group['description'] ); ?></p>
+	<?php endif; ?>
 
 	<ul>
-		<li>Tickets sold: <?php echo number_format_i18n( $wpcs['tickets_sold'] ); ?></li>
-		<li>Tickets refunded: <?php echo number_format_i18n( $wpcs['tickets_refunded'] ); ?></li>
-	</ul>
-
-	<table class="striped widefat but-not-too-wide">
-		<thead>
-			<tr>
-				<td>Currency</td>
-				<td>Gross Revenue</td>
-				<td>Discounts</td>
-				<td>Refunds</td>
-				<td>Net Revenue</td>
-				<td>Estimated Value in USD *</td>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ( array_keys( $wpcs['net_revenue_by_currency'] ) as $currency ) : ?>
-				<tr>
-					<td><?php echo esc_html( $currency ); ?></td>
-					<td class="number"><?php echo number_format_i18n( $wpcs['gross_revenue_by_currency'][ $currency ] ); ?></td>
-					<td class="number"><?php echo number_format_i18n( $wpcs['discounts_by_currency'][ $currency ] ); ?></td>
-					<td class="number"><?php echo number_format_i18n( $wpcs['amount_refunded_by_currency'][ $currency ] ); ?></td>
-					<td class="number"><?php echo number_format_i18n( $wpcs['net_revenue_by_currency'][ $currency ] ); ?></td>
-					<td class="number">
-						<?php echo number_format_i18n( $wpcs['converted_net_revenue'][ $currency ] ); ?>
-						<?php if ( $wpcs['net_revenue_by_currency'][ $currency ] > 0 && $wpcs['converted_net_revenue'][ $currency ] === 0 ) : $asterisk2 = true; ?>
-							**
-						<?php endif; ?>
-					</td>
-				</tr>
-			<?php endforeach; ?>
-			<tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td>Total: </td>
-				<td class="number total"><?php echo number_format_i18n( $wpcs['total_converted_revenue'] ); ?></td>
-			</tr>
-		</tbody>
-	</table>
-<?php endif; ?>
-
-<?php if ( ! empty( $non_wpcs['net_revenue_by_currency'] ) ) : ?>
-	<h3>
-		Non-WPCS Ticket Revenue
-		<?php if ( $wordcamp_name ) : ?>
-			for <?php echo esc_html( $wordcamp_name ); ?>
-		<?php endif; ?>
-		<?php if ( $start_date->format( 'Y-m-d' ) === $end_date->format( 'Y-m-d' ) ) : ?>
-			on <?php echo esc_html( $start_date->format( 'M jS, Y' ) ); ?>
-		<?php else : ?>
-			between <?php echo esc_html( $start_date->format( 'M jS, Y' ) ); ?> and <?php echo esc_html( $end_date->format( 'M jS, Y' ) ); ?>
-		<?php endif; ?>
-	</h3>
-
-	<p class="description">These numbers are based on the assumption that the currencies used in these transactions are not supported by PayPal, and thus the ticket sales are not through WPCS.</p>
-
-	<ul>
-		<li>Tickets sold: <?php echo number_format_i18n( $non_wpcs['tickets_sold'] ); ?></li>
-		<li>Tickets refunded: <?php echo number_format_i18n( $non_wpcs['tickets_refunded'] ); ?></li>
+		<li>Tickets sold: <?php echo number_format_i18n( $group['tickets_sold'] ); ?></li>
+		<li>Tickets refunded: <?php echo number_format_i18n( $group['tickets_refunded'] ); ?></li>
 	</ul>
 
 	<table class="striped widefat but-not-too-wide">
@@ -107,16 +52,16 @@ $asterisk2 = false;
 		</tr>
 		</thead>
 		<tbody>
-		<?php foreach ( array_keys( $non_wpcs['net_revenue_by_currency'] ) as $currency ) : ?>
+		<?php foreach ( array_keys( $group['net_revenue_by_currency'] ) as $currency ) : ?>
 			<tr>
 				<td><?php echo esc_html( $currency ); ?></td>
-				<td class="number"><?php echo number_format_i18n( $non_wpcs['gross_revenue_by_currency'][ $currency ] ); ?></td>
-				<td class="number"><?php echo number_format_i18n( $non_wpcs['discounts_by_currency'][ $currency ] ); ?></td>
-				<td class="number"><?php echo number_format_i18n( $non_wpcs['amount_refunded_by_currency'][ $currency ] ); ?></td>
-				<td class="number"><?php echo number_format_i18n( $non_wpcs['net_revenue_by_currency'][ $currency ] ); ?></td>
+				<td class="number"><?php echo number_format_i18n( $group['gross_revenue_by_currency'][ $currency ] ); ?></td>
+				<td class="number"><?php echo number_format_i18n( $group['discounts_by_currency'][ $currency ] ); ?></td>
+				<td class="number"><?php echo number_format_i18n( $group['amount_refunded_by_currency'][ $currency ] ); ?></td>
+				<td class="number"><?php echo number_format_i18n( $group['net_revenue_by_currency'][ $currency ] ); ?></td>
 				<td class="number">
-					<?php echo number_format_i18n( $non_wpcs['converted_net_revenue'][ $currency ] ); ?>
-					<?php if ( $non_wpcs['net_revenue_by_currency'][ $currency ] > 0 && $non_wpcs['converted_net_revenue'][ $currency ] === 0 ) : $asterisk2 = true; ?>
+					<?php echo number_format_i18n( $group['converted_net_revenue'][ $currency ] ); ?>
+					<?php if ( $group['net_revenue_by_currency'][ $currency ] > 0 && $group['converted_net_revenue'][ $currency ] === 0 ) : $group['asterisk2'] = $asterisk2 = true; ?>
 						**
 					<?php endif; ?>
 				</td>
@@ -128,74 +73,25 @@ $asterisk2 = false;
 			<td></td>
 			<td></td>
 			<td>Total: </td>
-			<td class="number total"><?php echo number_format_i18n( $non_wpcs['total_converted_revenue'] ); ?></td>
+			<td class="number total">
+				<?php echo number_format_i18n( $group['total_converted_revenue'] ); ?>
+				<?php if ( isset( $group['asterisk2'] ) ) : ?>
+					**
+				<?php endif; ?>
+			</td>
 		</tr>
 		</tbody>
 	</table>
-<?php endif; ?>
-
-<?php if ( ! empty( $wpcs['net_revenue_by_currency'] ) && ! empty( $non_wpcs['net_revenue_by_currency'] ) ) : ?>
-	<h3>
-		Total Ticket Revenue
-		<?php if ( $wordcamp_name ) : ?>
-			for <?php echo esc_html( $wordcamp_name ); ?>
-		<?php endif; ?>
-		<?php if ( $start_date->format( 'Y-m-d' ) === $end_date->format( 'Y-m-d' ) ) : ?>
-			on <?php echo esc_html( $start_date->format( 'M jS, Y' ) ); ?>
-		<?php else : ?>
-			between <?php echo esc_html( $start_date->format( 'M jS, Y' ) ); ?> and <?php echo esc_html( $end_date->format( 'M jS, Y' ) ); ?>
-		<?php endif; ?>
-	</h3>
-
-	<ul>
-		<li>Tickets sold: <?php echo number_format_i18n( $total['tickets_sold'] ); ?></li>
-		<li>Tickets refunded: <?php echo number_format_i18n( $total['tickets_refunded'] ); ?></li>
-	</ul>
-
-	<table class="striped widefat but-not-too-wide">
-		<thead>
-		<tr>
-			<td>Currency</td>
-			<td>Gross Revenue</td>
-			<td>Discounts</td>
-			<td>Refunds</td>
-			<td>Net Revenue</td>
-			<td>Estimated Value in USD *</td>
-		</tr>
-		</thead>
-		<tbody>
-		<?php foreach ( array_keys( $total['net_revenue_by_currency'] ) as $currency ) : ?>
-			<tr>
-				<td><?php echo esc_html( $currency ); ?></td>
-				<td class="number"><?php echo number_format_i18n( $total['gross_revenue_by_currency'][ $currency ] ); ?></td>
-				<td class="number"><?php echo number_format_i18n( $total['discounts_by_currency'][ $currency ] ); ?></td>
-				<td class="number"><?php echo number_format_i18n( $total['amount_refunded_by_currency'][ $currency ] ); ?></td>
-				<td class="number"><?php echo number_format_i18n( $total['net_revenue_by_currency'][ $currency ] ); ?></td>
-				<td class="number">
-					<?php echo number_format_i18n( $total['converted_net_revenue'][ $currency ] ); ?>
-					<?php if ( $total['net_revenue_by_currency'][ $currency ] > 0 && $total['converted_net_revenue'][ $currency ] === 0 ) : $asterisk2 = true; ?>
-						**
-					<?php endif; ?>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td>Total: </td>
-			<td class="number total"><?php echo number_format_i18n( $total['total_converted_revenue'] ); ?></td>
-		</tr>
-		</tbody>
-	</table>
-<?php endif; ?>
+	<?php $groups ++; ?>
+<?php endforeach; ?>
 
 <?php if ( ! empty( $total['net_revenue_by_currency'] ) ) : ?>
-	<p class="description">* Estimate based on exchange rates for <?php echo esc_html( $end_date->format( 'M jS, Y' ) ); ?></p>
-	<?php if ( $asterisk2 ) : ?>
-		<p class="description">** Currency exchange rate not available.</p>
-	<?php endif; ?>
+	<p class="description">
+		* Estimate based on exchange rates for <?php echo esc_html( $end_date->format( 'M jS, Y' ) ); ?>
+		<?php if ( $asterisk2 ) : ?>
+			<br />** Currency exchange rate not available.
+		<?php endif; ?>
+	</p>
 <?php else : ?>
 	<p>
 		No data
